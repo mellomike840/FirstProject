28f786fc2b7e8e58647ef2228101e12bcc8fc0eb
Alex Rottlaender <AlexRottlaender@gmail.com>
2018-10-09 12:20:43 -0600

    Added model group and renamed files

---
+++ FirstProject/Model/ColorTool.swift
@@ -0,0 +1,16 @@
+//
+//  ColorTool.swift
+//  FirstProject
+//
+//  Created by Rottlaender, Alex on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+
+import Foundation  //access to swift
+import UIKit
+
+public class ColorTool
+{
+    
+}
