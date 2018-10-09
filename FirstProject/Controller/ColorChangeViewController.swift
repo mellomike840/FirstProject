f2bde5ef4f8000c46765ea1e175b7cab8b2567e1
Alex Rottlaender <AlexRottlaender@gmail.com>
2018-10-09 13:46:23 -0600

    Put Button On Screen

--- FirstProject/Controller/ColorChangeViewController.swift
+++ FirstProject/Controller/ColorChangeViewController.swift
@@ -10,13 +10,26 @@ import UIKit
 
 public class ColorChangeViewController: UIViewController
 {
-
+    private lazy var myColor : ColorTool = ColorTool()
+    
+    @IBOutlet weak var firstButton : UIButton!
+    
+    //because its a public class it has a public method for use externally
+    //private methods are only used internally
     public override func viewDidLoad() -> Void
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func colorClick(_ sender: UIButton) -> Void
+    {
+        //assigning this things views background color to a random color
+        self.view.backgroundColor = myColor.createRandomColor()
+        firstButton.backgroundColor = myColor.createRandomColor()
+    }
+    
+    
 
 }
 
--- FirstProject/Model/ColorTool.swift
+++ FirstProject/Model/ColorTool.swift
@@ -16,9 +16,11 @@ public class ColorTool
     
     public func createRandomColor() -> UIColor
     {
-        let redPercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
-        let bluePercent : CGFloat = CGFloat ((arc4random() % 256) / 255 )
-        let greenPercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
+        //taking the number between 0 and 255 and treating it as a double, because you are dividing by a double.
+        let redPercent : CGFloat = CGFloat (Double ((arc4random() % 256)) / 255.00)
+        //GUI elements usee CGFloat types, not int or double.
+        let bluePercent : CGFloat = CGFloat (Double ((arc4random() % 256)) / 255.00)
+        let greenPercent : CGFloat = CGFloat (Double ((arc4random() % 256)) / 255.00)
         
         return UIColor(red: redPercent, green: greenPercent, blue: bluePercent, alpha: CGFloat(100))
     }
